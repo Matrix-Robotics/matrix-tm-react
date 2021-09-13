d806e32d3b1576f19a8c8b9f574502ab31ee0392
Kevin Cheng <kevinphy@gmail.com>
2021-09-13 13:37:14 +0800

    first testing layout

--- src/Capture.js
+++ src/Capture.js
@@ -1,30 +1,32 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import CardHeader from '@material-ui/core/CardHeader';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import IconButton from '@material-ui/core/IconButton';
 import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://matrixrobotics.com/">
-        MATRIX Robotics
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+
+const options = [
+  'None',
+  'Pyxis',
+  'Atria',
+  'Callisto',
+  'Sedna',
+  'Titania',
+  'Triton',
+  'Umbriel',
+];
+
+const ITEM_HEIGHT = 80;
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -46,6 +48,11 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'column',
   },
+  cardCenter: {
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+  },
   cardMedia: {
     paddingTop: '56.25%', // 16:9
   },
@@ -60,62 +67,98 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Capture() {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  function ClassCard() {
+    return (
+      <React.Fragment>
+        <Grid item xs={12}>
+          <Card className={classes.card}>
+            <CardHeader
+              action={
+                <div>
+                  <IconButton aria-label="settings" onClick={handleClick}>
+                    <MoreVertIcon />
+                  </IconButton>
+                  <Menu
+                    id="long-menu"
+                    anchorEl={anchorEl}
+                    getContentAnchorEl={null}
+                    keepMounted
+                    anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                    transformOrigin={{ vertical: "top", horizontal: "center" }}
+                    open={open}
+                    onClose={handleClose}
+                    PaperProps={{
+                      style: {
+                        maxHeight: ITEM_HEIGHT * 4.5,
+                        width: '20ch',
+                      },
+                    }}
+                  >
+                    {options.map((option) => (
+                      <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
+                        {option}
+                      </MenuItem>
+                    ))}
+                  </Menu>
+                </div>
+              }
+              title="Class 1"
+            />
+            <CardContent className={classes.cardContent}>
+              <Typography>
+                This is a media card. You can use this section to describe the content.
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" color="primary">
+                View
+              </Button>
+              <Button size="small" color="primary">
+                Edit
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </React.Fragment>
+    );
+  };
 
   return (
     <React.Fragment>
       <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            MATRIX Tempurad
-          </Typography>
-        </Toolbar>
-      </AppBar>
       <main>
         <Container className={classes.cardGrid} maxWidth="xl">
-          <Grid container spacing={10}>
-            <Grid item key={1} xs={12} sm={6} md={6}>
+          <Grid container spacing={5} alignItems="center">
+            <Grid container item key={1} xs={12} sm={6} md={6} spacing={5} justifyContent="center" direction="column">
+              <ClassCard />
+              <ClassCard />
               <Card className={classes.card}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  image="https://source.unsplash.com/random"
-                  title="Image title"
-                />
-                <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Heading
-                  </Typography>
-                  <Typography>
-                    This is a media card. You can use this section to describe the content.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="large" color="primary">
-                    View
-                  </Button>
-                  <Button size="small" color="primary">
-                    Edit
-                  </Button>
-                </CardActions>
+                <Button size="large" color="primary">
+                  Add new class
+                </Button>
               </Card>
             </Grid>
             <Grid item key={2} xs={12} sm={6} md={2}>
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  image="https://source.unsplash.com/random"
-                  title="Image title"
-                />
+              <Card className={classes.cardCenter}>
+                <CardHeader classes="title" title="Training"/>
                 <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Heading
-                  </Typography>
                   <Typography>
                     This is a media card. You can use this section to describe the content.
                   </Typography>
                 </CardContent>
                 <CardActions>
-                  <Button size="large" color="primary">
+                  <Button size="small" color="primary">
                     View
                   </Button>
                   <Button size="small" color="primary">
@@ -125,22 +168,15 @@ export default function Capture() {
               </Card>
             </Grid>
             <Grid item key={3} xs={12} sm={6} md={4}>
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  image="https://source.unsplash.com/random"
-                  title="Image title"
-                />
+              <Card className={classes.cardCenter}>
+                <CardHeader title="Preview"/>
                 <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Heading
-                  </Typography>
                   <Typography>
                     This is a media card. You can use this section to describe the content.
                   </Typography>
                 </CardContent>
                 <CardActions>
-                  <Button size="large" color="primary">
+                  <Button size="small" color="primary">
                     View
                   </Button>
                   <Button size="small" color="primary">
@@ -152,17 +188,6 @@ export default function Capture() {
           </Grid>
         </Container>
       </main>
-      {/* Footer */}
-      <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-          Something here to give the footer a purpose!
-        </Typography>
-        <Copyright />
-      </footer>
-      {/* End footer */}
     </React.Fragment>
   );
 }
