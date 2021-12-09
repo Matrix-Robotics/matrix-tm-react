e9aef7e9b2596c2060dfbedd7e4630abd2b92e8c
RUI-LONG <Ryan@kkitc.net>
2021-12-09 14:30:53 +0800

    Adjust learning rate limits

--- src/Interface.js
+++ src/Interface.js
@@ -610,7 +610,7 @@ export default function Interface() {
                     value={lRateValue}
                     variant="outlined"
                     size="small"
-                    inputProps={{ step: 0.001 }}
+                    inputProps={{ min: 0, max: 1, step: 0.001 }}
                     onChange={(e) => setLRateValue(parseFloat(e.target.value).toFixed(3))}
                   />
 
